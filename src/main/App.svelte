25fe615cb274cc49ac15048262737a097cd0f0ca
Leone <leeman2016@gmail.com>
2021-06-07 16:01:55 +0300

    Add svelte support

---
+++ src/main/App.svelte
@@ -0,0 +1,22 @@
+<script>
+    import SvelteComponent from '../SvelteComponent.svelte'
+    import {vd} from "../AbstractDOM";
+
+    let count = 0
+
+    const CounterInfo = ({c = 1}) => vd('div', {}, `${c}`)
+</script>
+
+<h1>Counter</h1>
+<SvelteComponent dom={CounterInfo} props={{c: count}} />
+<button on:click={e => count = count+1}>+</button>
+
+<h1>Simple</h1>
+<SvelteComponent dom={vd('div', {}, [
+        'Visit us ',
+        vd('a', {href: 'http://google.com'}, 'here'),
+        '.'
+    ])} />
+
+<h1>With Events</h1>
+<SvelteComponent dom={vd('button', {onclick: e => alert('Clicked!')}, 'Click Me')} />
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 var path = require('path')
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import svelte from '@sveltejs/vite-plugin-svelte'
 
 
 export default defineConfig({
-    plugins: [reactRefresh()],
+    plugins: [reactRefresh(), svelte()],
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
