0fd5e66b3ecaf65049041447f160f316ef9491fa
Leone <leeman2016@gmail.com>
2021-06-08 19:46:18 +0300

    Add state abstraction with samples

---
+++ src/State.ts
@@ -0,0 +1,33 @@
+import {AbstractDomNode} from "./AbstractDOM";
+import {OneOrMany} from "boost-web-core";
+
+export type StateSubscription = string
+
+export abstract class AbstractReadableState<T = any> {
+    abstract bind<TP = T>(expr?: (state: T) => TP): AbstractReadableState<TP>
+    abstract subscribe(subscriber: any): StateSubscription
+    abstract unsubscribe(s: StateSubscription)
+    abstract get(): T
+}
+
+export abstract class AbstractWritableState<T = any> extends AbstractReadableState<T> {
+    abstract set(newVal: T): void
+    abstract update(reducer: (prev: T) => T): void
+    abstract mutate(reducer: (prev: T) => void): void
+}
+
+type StateMapping<TVal> = (forState: AbstractWritableState<TVal>) => OneOrMany<AbstractDomNode>
+
+export class AbstractDomNodeWithState<TVal = any> {
+    initialState: TVal
+    stateMapping: StateMapping<TVal>
+
+    constructor(_initialState: TVal, _stateMap: StateMapping<TVal>) {
+        this.initialState = _initialState
+        this.stateMapping = _stateMap
+    }
+}
+
+export function withState<T>(initialValue: T, stateMapping: StateMapping<T>): AbstractDomNodeWithState<T> {
+    return new AbstractDomNodeWithState(initialValue, stateMapping)
+}
